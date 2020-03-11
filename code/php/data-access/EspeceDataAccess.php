af11a5abba195b9b6385aa3f044ec27cac12386f
pierre <pentier.pierre@yahoo.fr>
2020-03-11 21:47:28 +0100

    [pierre] travail sur l'affichage les types d'animaux dans le formulaire de recherche

--- code/php/data-access/EspeceDataAccess.php
+++ code/php/data-access/EspeceDataAccess.php
@@ -1,5 +1,14 @@
 <?php
     class EspeceDataAccess{
 
+        public function afficherType(){
+            $db= mysqli_init();
+            mysqli_real_connect($db, 'localhost','root','','bddanimaux');
+            $rs= mysqli_query($db, "SELECT nom_espece FROM espece");
+            $data=mysqli_fetch_all($rs, MYSQLI_ASSOC);
+            mysqli_close($db);
+            return $data;
+        }
+
     }
 ?>
--- code/php/service/EspeceService.php
+++ code/php/service/EspeceService.php
@@ -1,5 +1,13 @@
 <?php
+
+include_once('../data-access/EspeceDataAccess.php');
     class EspeceService{
+        public function afficherType(){
+            $especeDataAccess = new EspeceDataAccess();
+            $data = $especeDataAccess->afficherType();
+            return $data;            
+        }
+
 
     }
 ?>
