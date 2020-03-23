56c38d5b87a528cbce30041d249fdb7a4040f7f6
Julian N <noeljulian7@gmail.com>
2020-03-23 17:20:42 +0100

    correction

--- code/php/data-access/AnimauxFavorisDataAccess.php
+++ code/php/data-access/AnimauxFavorisDataAccess.php
@@ -4,17 +4,12 @@ class AnimauxFavorisDataAcces implements InterfaceDao {
 
     public function daoSelectAll(){}
     public function daoSelectAllUserFavouritesAnimals(){}
-<<<<<<< HEAD
-    public function daoSelect($id){}
-=======
     public function daoSelect(int $id){}
->>>>>>> Correction type variable
     public function daoCount(){}
     public function daoAdd($object){}
     public function daoSearch($search){}
     public function daoUpdate($parametres){}
     public function daoDelete($nom){}
-
 }
 
 
