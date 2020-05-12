a625e6b516a4941fdb442f957528fa920cbbf2ad
Julian N <noeljulian7@gmail.com>
2020-05-12 15:34:26 +0200

    add infecteParService

--- code/php/service/InfecteParService.php
+++ code/php/service/InfecteParService.php
@@ -6,7 +6,9 @@
         public function serviceSelectAll(){}
         public function serviceSelect($id){}
         public function serviceCount(){}
-        public function serviceAdd($var){}
+        public function serviceAdd($infectePar){
+            $this->getDataAccessObject()->daoAdd($infectePar);
+        }
         public function serviceSearch($search){}
         public function serviceUpdate($post){}
         public function serviceDelete($nom){}
