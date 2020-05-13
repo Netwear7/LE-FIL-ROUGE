993aef014ee2822a1384437aa8874f165c0dfc98
Julian N <noeljulian7@gmail.com>
2020-05-13 16:43:39 +0200

    Création especeAvoirMaladieDataAccess

--- code/php/data-access/EspeceAvoirMaladieDataAccess.php
+++ code/php/data-access/EspeceAvoirMaladieDataAccess.php
@@ -1,5 +1,41 @@
 <?php
-    class EspeceAvoirDataAccess{
 
+    include_once '../data-access/LogBdd.php';
+    include_once '../Interfaces/InterfaceDao.php';
+
+    class EspeceAvoirMaladieDataAccess extends LogBdd implements InterfaceDao{
+
+        public function daoSelectAll(){
+            $mysqli = $this->connexion();
+            $rs = $mysqli->query('SELECT * from espece_avoir_maladie');
+            $data = $rs->fetch_all(MYSQLI_ASSOC);
+            $rs->free();
+            $this->deconnexion($mysqli);
+            return $data;
+        }
+        public function daoSelect($id){
+
+        }
+        public function daoCount(){
+
+        }
+        public function daoAdd($especeAvoirMaladie){
+            $mysqli = $this->connexion();
+            $espece = $especeAvoirMaladie->getIdEspece();
+            $maladie = $especeAvoirMaladie->getIdMaladie();
+            $stmt = $mysqli->prepare("INSERT INTO espece_avoir_maladie(ID_MALADIE, ID_ESPECE) VALUES(?,?)");
+            $stmt->bind_param("ss", $maladie, $espece);
+            $stmt->execute();
+            $this->deconnexion($mysqli);
+        }
+        public function daoSearch($search){
+
+        }
+        public function daoUpdate($parametres){
+
+        }
+        public function daoDelete($nom){
+
+        }
     }
 ?>
